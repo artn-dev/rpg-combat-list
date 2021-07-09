a4f99a02ec640a70cb84e6bd70e0ecebca772bd6
artn-dev <azemarteixeira@gmail.com>
2021-07-09 11:47:21 -0300

    Add visibility

--- components/Combatant.tsx
+++ components/Combatant.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { CombatContext, Combatant } from '../contexts/CombatContext'
 import {
     ListItem,
@@ -8,47 +8,81 @@ import {
     Avatar,
     IconButton,
     Typography,
-    Tooltip
+    Tooltip,
+    Theme
 } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
+import VisibilityIcon from '@material-ui/icons/Visibility'
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff'
+import { createStyles, makeStyles } from '@material-ui/styles'
+import clsx from 'clsx'
 
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        deleted: {
+            textDecoration: "line-through",
+            textDecorationColor: "rgba(0, 0, 0, 0.54)",
+        },
+    }),
+)
 
 const CombatantNode = ({ name, initiative, id }: Combatant) => {
     const { deleteCombatantById } = useContext(CombatContext)
 
+    const classes = useStyles()
+
+    const [isInCombat, setIsInCombat] = useState<boolean>(true)
+
     const selfDelete = () => deleteCombatantById(id)
 
+    const toggleIsInCombat = () => {
+        setIsInCombat(!isInCombat)
+    }
+
     return (
-        <>
-            <ListItem divider={true}>
-
-                <ListItemAvatar>
-                    <Avatar>
-                        <Typography align="center" variant="h5">
-                            {initiative}
-                        </Typography>
-                    </Avatar>
-                </ListItemAvatar>
-
-                <ListItemText
-                    primary={name}
-                    primaryTypographyProps={{ variant: "h5" }}
-                />
-
-                <ListItemSecondaryAction>
-                    <Tooltip title="Delete">
-                        <IconButton
-                            edge="end"
-                            aria-label="delete"
-                            onClick={selfDelete}
+        <ListItem divider={true}>
+            <ListItemAvatar>
+                <Avatar>
+                    <Typography align="center" variant="h5">
+                        {initiative}
+                    </Typography>
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+                className={clsx({
+                    [classes.deleted]: !isInCombat,
+                })}
+                primary={name}
+                primaryTypographyProps={{
+                    variant: "h5",
+                    color: isInCombat ? "textPrimary" : "textSecondary",
+                }}
+            />
+            <ListItemSecondaryAction>
+                <Tooltip title={isInCombat ? "In Combat" : "Out of Combat"}>
+                    <IconButton
+                        edge="end"
+                        aria-label="toggle-in-combat"
+                        onClick={toggleIsInCombat}
                         >
-                            <DeleteIcon />
-                        </IconButton>
-                    </Tooltip>
-                </ListItemSecondaryAction>
-
-            </ListItem>
-        </>
+                        { isInCombat 
+                            ? <VisibilityIcon />
+                            : <VisibilityOffIcon />
+                        }
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Delete">
+                    <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={selfDelete}
+                    >
+                        <DeleteIcon />
+                    </IconButton>
+                </Tooltip>
+            </ListItemSecondaryAction>
+        </ListItem>
     )
 }
 
